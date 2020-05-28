11fc3f53b7ca8d56928c48e57a277d10127034b5
tsbarrett89 <tsbarrett89@gmail.com>
2020-05-28 14:26:42 -0600

    choose class functional

--- index.js
+++ index.js
@@ -30,60 +30,112 @@ const axeTexture = PIXI.Texture.from("images/axe.png");
 
 const cannonTexture = PIXI.Texture.from("images/cannon.png");
 const bombTexture = PIXI.Texture.from("images/bomb.png");
+const crossbowTexture = PIXI.Texture.from("images/crossbow.png")
+
+const templeFloorTexture = PIXI.Texture.from("images/templeFloor.jpg")
+
 const woodTexture = PIXI.Texture.from("images/wood.png");
 
 
 // Create Containers
 
+const startPage = new PIXI.Container();
+const classChoicePage = new PIXI.Container();
+const actOne = new PIXI.Container();
 
 
 // Create Sprites
 
+let chosenClass = null;
+
 const archer = new PIXI.Sprite(archerTexture);
-archer.x = (app.renderer.width / 2) - 60;
-archer.y = app.renderer.height / 2;
-archer.anchor.set(1)
+archer.x = (app.renderer.width / 2) - 300;
+archer.y = (app.renderer.height / 2) + 100;
+archer.anchor.set(0.5)
 archer.scale.set(-0.2, 0.2);
 archer.interactive = true;
-archer.on('click', archerChoose)
+archer.on('click', () => {
+    chooseClass(archer)
+})
 
+const warrior = new PIXI.Sprite(warriorTexture);
+warrior.x = (app.renderer.width / 2);
+warrior.y = (app.renderer.height / 2) + 100;
+warrior.anchor.set(0.5);
+warrior.scale.set(-0.2, 0.2);
+warrior.interactive = true;
+warrior.on('click', () => {
+    chooseClass(warrior)
+})
 
-function archerChoose() {
-    console.log("click happens")
-    archer.y = 200
-}
+const wizard = new PIXI.Sprite(wizardTexture);
+wizard.x = (app.renderer.width / 2) + 300;
+wizard.y = (app.renderer.height / 2) + 100;
+wizard.anchor.set(0.5);
+wizard.scale.set(-0.2, 0.2);
+wizard.interactive = true;
+wizard.on('click', () => {
+    chooseClass(wizard)
+})
 
-cannonLeft = new PIXI.Sprite(cannonTexture);
+const cannonLeft = new PIXI.Sprite(cannonTexture);
 cannonLeft.x = 20;
 cannonLeft.y = app.renderer.height;
 cannonLeft.anchor.set(1, 1);
 cannonLeft.scale.set(-0.2, 0.2);
 
-cannonRight = new PIXI.Sprite(cannonTexture)
+const cannonRight = new PIXI.Sprite(cannonTexture)
 cannonRight.x = app.renderer.width - 20;
 cannonRight.y = app.renderer.height;
 cannonRight.anchor.set(1, 1);
 cannonRight.scale.set(0.2);
 
-bombLeft = new PIXI.Sprite(bombTexture)
+const bombLeft = new PIXI.Sprite(bombTexture)
 bombLeft.x = cannonLeft.x + 110;
 bombLeft.y = cannonLeft.y - 90;
 bombLeft.scale.set(0.1);
 bombLeft.anchor.set(0.5);
 
-bombRight = new PIXI.Sprite(bombTexture)
+const bombRight = new PIXI.Sprite(bombTexture)
 bombRight.x = cannonRight.x - 110;
 bombRight.y = cannonRight.y - 90;
 bombRight.scale.set(0.1);
 bombRight.anchor.set(0.5);
 
-// Message Board elements
+// Messages
 
-// const 
+const messageStyle = new PIXI.TextStyle({
+    fill: 'white'
+})
+const chooseClassMessage = new PIXI.Text("Choose your class", messageStyle)
+chooseClassMessage.x = app.renderer.width / 2;
+chooseClassMessage.y = (app.renderer.height / 2) - 100;
+chooseClassMessage.anchor.set(0.5);
 
 
-app.stage.addChild(archer);
+// Scene Changes
+
+function chooseClass(classChoice){
+    chosenClass = classChoice
+    app.stage.removeChild(startPage)
+    classChoicePage.addChild(chosenClass)
+    app.stage.addChild(classChoicePage)
+    gsap.to(chosenClass, {duration: 1, x: app.renderer.width / 2, y: app.renderer.height / 2})
+}
+
+function actOneStart(){
+    
+}
+
+
+// Start stage
+startPage.addChild(archer)
+startPage.addChild(warrior)
+startPage.addChild(wizard)
+startPage.addChild(chooseClassMessage)
+
+app.stage.addChild(startPage);
 
 
 const interface = document.getElementById('interface')
-const testButton = document.getElementById('test')
+const testButton = document.getElementById('test')                                                                                                  
